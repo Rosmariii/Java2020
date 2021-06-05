44fa1adc7316c4506531fa7b56a22210e3de7db8
Rosmari <rosmarifernandez@hotmail.com>
2021-06-05 20:22:13 -0300

    ejercicios

---
+++ complementarios1/Ejercicio1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String nombre = scan.nextLine();
+        System.out.println("Hola " + nombre);
+        scan.close();
+    }
+} 
---
+++ complementarios1/Ejercicio5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int num1 = 0;
+        int num2 = 0;
+
+        System.out.println("ingrese el primer numero: ");
+        num1 = leer.nextInt();
+
+        System.out.println("ingrese el segundo numero: ");
+        num2 = leer.nextInt();
+        leer.close();
+        
+        int sum = 0; 
+        int i = 1;
+
+        for (i=1; i<=num2; i++) {
+            sum = sum+num1;    
+        }
+        System.out.println(sum);
+    
+    }
+}
